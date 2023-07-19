323bc4004acb2aec33b962448294078115d233ba
ankundingbfmerlin67 <138851234+ankundingbfmerlin67@users.noreply.github.com>
2023-07-19 13:39:56 +0100

    Create net-http.go

---
+++ net-http.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8000", nil)
+}
